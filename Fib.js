a32e8558386b2c5f6c6c86287f8b1392f439a06a
Paul Kelly <91570092+pk5280@users.noreply.github.com>
2021-11-05 10:46:14 -0600

    Update Fib.js

--- Fib.js
+++ Fib.js
@@ -1,4 +1,4 @@
-
+console.time('fib1')
 function fib1(n){
     //Brute-Force
     let x = 1;
@@ -15,12 +15,16 @@ function fib1(n){
     }
     return sum
 }
-
+console.timeEnd('fib1')
+console.time('fib2')
 function fib2(n) {
     //Binet's Formula
     return Math.round((Math.pow((1+Math.sqrt(5))/2, n) - Math.pow((1-Math.sqrt(5))/2, n)) / Math.sqrt(5));
 }
+console.timeEnd('fib2')
 
-console.log(fib1(1), fib1(2), fib1(3), fib1(4), fib1(5), fib1(16))
-console.log(fib2(1), fib2(2), fib2(3), fib2(4), fib2(5), fib2(16))
+// nth digit of Fibonacci Sequence
+let n = 6
+console.log(fib1(n))
+console.log(fib2(n))
 
